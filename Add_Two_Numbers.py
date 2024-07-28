024134ed4ce3333e6bdfdda2c9b78dabdcf198ab
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-28 23:48:29 +0530

    Add files via upload

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,113 @@
+# a, b, c, d, e, f = None, None, None, None, None, None
+
+# # def add(a,b,c,d,e,f):
+# def add():
+#     sum = a + b+ c + d + e + f
+#     return sum 
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# e = int(input())
+# f = int(input())
+
+# # print(add(a,b,c,d,e,f))
+
+# # print(add(a,b,c))
+
+# print(add())
+
+
+
+
+
+# # Global variables
+# a, b, c, d, e, f = None, None, None, None, None, None
+
+# def add():
+#     return a + b + c + d + e + f
+
+# # Assigning values to global variables
+# a = int(input("Enter value for a: "))
+# b = int(input("Enter value for b: "))
+# c = int(input("Enter value for c: "))
+# d = int(input("Enter value for d: "))
+# e = int(input("Enter value for e: "))
+# f = int(input("Enter value for f: "))
+
+# print(add())
+
+
+
+
+
+
+# # def add(a,b,c,d,e,f):
+# def add():
+#     a, b, c, d, e, f = 0,0,0,0,0,0
+#     sum = a + b+ c + d + e + f
+#     return sum 
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# e = int(input())
+# f = int(input())
+
+# # print(add(a,b,c,d,e,f))
+
+# # print(add(a,b,c))
+
+# print(add())
+
+
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# e = int(input())
+# f = int(input())
+
+# # def add(a,b,c,d,e,f):
+# def add():
+#     # a, b, c, d, e, f = 0,0,0,0,0,0
+#     sum = a + b+ c + d + e + f
+#     return sum 
+
+
+# # print(add(a,b,c,d,e,f))
+
+# # print(add(a,b,c))
+
+# print(add())
+
+
+
+
+
+
+
+# def add(a,b,c,d,e,f):
+def add():
+    # a, b, c, d, e, f = 0,0,0,0,0,0
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    e = int(input())
+    f = int(input())
+    sum = a + b+ c + d + e + f
+    return sum 
+
+
+# print(add(a,b,c,d,e,f))
+
+# print(add(a,b,c))
+
+print(add())
