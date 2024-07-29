d9a8a739cdc59cbc23724ce9890c2cc98f2a99f0
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-29 10:13:10 +0530

    Add files via upload

---
+++ Find Maximum_of_two_numbers.py
@@ -0,0 +1,50 @@
+def max(a,b):
+    if a > b :
+        return a 
+    else:
+        return b 
+    
+
+print(max(115,86))
+
+print('\n')
+
+
+
+# Python program to find the
+# maximum of two numbers
+
+
+a = 2
+b = 4
+
+maximum = max(a, b)
+print(maximum)
+
+
+
+print('\n')
+
+# Python program to find the
+# maximum of two numbers
+    
+# Driver code
+a = 22
+b = 48
+
+# Use of ternary operator
+print(a if a >= b else b)
+
+
+
+
+print('\n')
+
+# python code to find maximum of two numbers
+
+a=2
+b=4
+maximum = lambda a,b:a if a > b else b
+print(f'{maximum(a,b)} is a maximum number')
+
+
