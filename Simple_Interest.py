2d0c60435a1861c9aa5b449fd4556bbb2034f2f0
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-07-31 19:17:46 +0530

    Add files via upload

---
+++ Simple_Interest.py
@@ -0,0 +1,42 @@
+### 31st July -- 10:17 Am
+
+#######   1  #########
+# p = int(input("Enter the principal "))
+# r = int(input("Enter the rate "))
+# t = int(input("Enter the time "))
+
+# def interest (p,r,t):
+#     s_i = (p * t * r)/100
+#     return s_i 
+
+# print(interest(p,r,t))
+
+
+###########  2  ##########
+
+# p = int(input("Enter the principal "))
+# r = int(input("Enter the rate "))
+# t = int(input("Enter the time "))
+
+# def interest ():
+#     s_i = (p * t * r)/100
+#     return s_i 
+
+# print(interest())
+
+
+
+#############  3  ############
+
+
+def interest ():
+    s_i = (p * t * r)/100
+    return s_i 
+
+
+
+p = int(input("Enter the principal "))
+r = int(input("Enter the rate "))
+t = int(input("Enter the time "))
+
+print(interest())
