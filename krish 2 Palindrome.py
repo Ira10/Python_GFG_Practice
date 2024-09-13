8512b53966c46d71982cf5c915a34f0aa4dc56b1
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-09-13 22:28:52 +0530

    Add files via upload

---
+++ krish 2 Palindrome.py
@@ -0,0 +1,73 @@
+def palindrome(n):     # 121 is a palindrome   ## 13th Sep  ## 12 mins
+    pal = n
+    new_ = 0
+
+    while n!= 0:
+        rem = n%10
+        new_ = new_*10 + rem
+        n = n //10
+
+    # return n , new_   # (0, 121)      
+        if new_ == pal:
+            print(pal, "and", new_, "is palindrome")
+        else:
+            print("no")
+
+print(palindrome(121))
+
+# no   ## for every instance of loop , if-else is printing
+# no
+# 121 and 121 is palindrome
+# None
+
+######## correct version  ###########
+
+def palindrome(n):
+
+    pal = n
+    new_ = 0
+
+    while n!= 0:
+        rem = n%10
+
+        new_ = new_*10 + rem
+
+        n = n //10
+
+    # return n , new_   # (0, 121)
+        
+    if new_ == pal:
+        print(pal, "and", new_, "is palindrome")
+    else:
+        print("no")
+
+
+print(palindrome(121))
+
+# 121 and 121 is palindrome
+# None
+
+
+# print(125%10)
+
+# print(1258//10)
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+            
